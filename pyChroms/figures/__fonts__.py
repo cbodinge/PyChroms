91030d89eebbd111ace9826a886b5eb248d8855e
Carter Bodinger <carterallenbodinger@yahoo.com>
2025-01-06 12:07:22 -0600

    Changed Font to Trebuchet MS

--- pyChroms/figures/__fonts__.py
+++ pyChroms/figures/__fonts__.py
@@ -1,7 +1,11 @@
 from PSVG import Font
 
-family = 'JetBrains Mono'
-font500 = Font(family, '500')
-font600 = Font(family, '600')
-font700 = Font(family, '700')
-font800 = Font(family, '800')
+# family = 'JetBrains Mono'
+# font500 = Font(family, '500')
+# font600 = Font(family, '600')
+# font700 = Font(family, '700')
+# font800 = Font(family, '800')
+
+family = 'Trebuchet MS'
+font500 = Font(family, '400')
+font600 = Font(family, '500')
